94e50aeb025d16b9b7a907f9deb104f05e46c4b3
Skyler Kweon <111268643+Kweonsuni@users.noreply.github.com>
2023-04-23 23:14:40 -0500

    debug with professor

--- servo.ino
+++ servo.ino
@@ -1,20 +1,28 @@
 #include <Servo.h>
 
-//Using 3 servos with 4 buttons
+//Using 4 servos with 4 buttons
 Servo myservo1;
 Servo myservo2;
 Servo myservo3;
+Servo myservo4;
 
 const int buttonPin1 = 2;
 const int buttonPin2 = 3;
 const int buttonPin3 = 4;
 const int buttonPin4 = 5;
 
+boolean done1 = false;
+boolean done2 = false;
+boolean done3 = false;
+
 void setup() {
+
+  Serial.begin(9600);
 
   myservo1.attach(8);
   myservo2.attach(9);
   myservo3.attach(10);
+  myservo4.attach(11);
 
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
@@ -25,38 +33,33 @@ void setup() {
   myservo1.write(0);
   myservo2.write(0);
   myservo3.write(0);
+  myservo4.write(0);
 }
-
-boolean done1 = false;
-boolean done2 = false;
-boolean done3 = false;
 
 void loop() {
   if (digitalRead(buttonPin1) == HIGH) {
     myservo1.write(90);
+    myservo2.write(90);
     done1 = true;
   } else {
     myservo1.write(0);
+    myservo2.write(0);
   }
   if (done1 == true) {
     if (digitalRead(buttonPin2) == HIGH) {
-      myservo2.write(90);
+      myservo3.write(90);
       done2 = true;
     } else {
-      myservo2.write(0);
+      myservo3.write(0);
     }
   }
   if (done2 == true) {
-    if (digitalRead(buttonPin3) == HIGH) {
-      myservo3.write(180);
-      done3 = true;
-    }
-  }
-  if (done3 == true) {
-    if (digitalRead(buttonPin4) == HIGH) {
-      myservo3.write(180);
-    } else {
-      myservo3.write(0);
+    Serial.print(digitalRead(buttonPin3));
+    Serial.println(digitalRead(buttonPin4));
+    if (digitalRead(buttonPin3) == HIGH && digitalRead(buttonPin4) == LOW) {
+      myservo4.write(180);
+    }else if (digitalRead(buttonPin3) == HIGH && digitalRead(buttonPin4) == HIGH){
+      myservo4.write(0);
     }
   }
 }
